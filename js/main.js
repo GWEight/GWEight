9e99af6fb1a8e2558e177766bcae2e1303930c48
GWEight <76634703+GWEight@users.noreply.github.com>
2020-12-30 23:10:54 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -47,27 +47,29 @@ tapFirstProj.addEventListener('click',()=>{
     aside.style.height = 'fit-content';
     setTimeout(() => {
         if(localStorage.language == 'En'){  
+            projectsBoxes.style.height = 'fit-content';
         projectsBoxes.innerHTML = `
         <div id="tapText" style="width: 100%; height: 100%; color: white;">
             <h2 style="text-align: center;">Tap</h2>
             <div style="" class="motherDivGenerator"> 
                 <img src="./assets/Games/Tap1.jpg" alt="" style="min-width: 510px; min-height: 510px">  
                 <p style="width: 310px; align-self: center;  margin: 0; text-align: center;">Tap is an addicting marksmanship game! Play it now, relax your brain and have a lot of fun passing the company!
-                You need to hit the maximum number of balls in order to achieve the highest result and go to the next level.</p>
+                You need to hit the maximum number of balls in order to achieve the highest result and go to the next level.<a style=" padding:50px 0;" href="ProjData/Tap.apk" download>Download</a></p>
             </div>
-            <div style=" padding:50px 0;" class="motherDivGenerator">             
-            <p style="width: 30%;">Features:
+            <div style="padding:50px 0;" class="motherDivGenerator">             
+            <p class="someTextwidthClas">Features:
                 1. Free game: many skins and bonuses
                 2. Simple ball control: tap the screen and fly the ball with one hand
                 3. Catchy graphics: neon blocks painted in different colors
                 4. Offline mode: there are absolutely no ads in offline mode
             </p>
-            <p style="width: 30%;">
+            <p class="someTextwidthClas">
             How to play:
                 1. Choose between casual, endless and level play
                 2. Match red balls that will give you an extra life
                 3. Find the best moment to hit
                 4. Try to get better results and get more achievements
+                <a href="projects.html" style ="padding:50px;" >Watch silent tutorial</a>
             </p>
             </div>
 
@@ -80,10 +82,8 @@ tapFirstProj.addEventListener('click',()=>{
 
             <div style=" width: 100%; padding:50px 0;" class="motherDivGenerator">
             <p>In this fun addicting game you can unlock different balls and themes of different levels (legendary ones have their own abilities)! So if you have a lot of free time and you are bored, start the game.
-
                 Just download the game and compete with your friends! Find out which one of you is better!
-                And if you encounter any problems, please write to us at <a href="mailto:gameweighteight@gmail.com" style="display:block;">[gameweighteight@gmail.com]</a> and we will help you.
-            </p>
+                And if you encounter any problems, please write to us at <i><a href="mailto:gameweighteight@gmail.com" style="display: unset; width: fit-content;"> gameweighteight@gmail.com </a></i> and we will help you.</p>
             </div>
 
             <div style = " padding:50px;" class="motherDivGenerator">
@@ -94,14 +94,18 @@ tapFirstProj.addEventListener('click',()=>{
 
 
             <div class="motherDivGenerator" style ="padding:50px;" >
-                <img src="./assets/Games/tapBar.png" alt="">
-                <p style="width: 40%;"> This is the in game panel. There are two states of the fast loading button here(Red - Off / Green - On), if it's on,
+                <img src="./assets/Games/tapBar.png" style="min-width: 450px;">
+                <p class="someTextwidthClas"> This is the in game panel. There are two states of the fast loading button here(Red - Off / Green - On), if it's on,
                 you will be loading directly selected game mode bypassing the main page, just off it by clicking on green button</p>
             </div>
 
             <div class="motherDivGenerator" style ="padding:50px; justify-content: center;" >
-                <p style ="width: 40%">And Pause, where you can go to home game directory, on/off music and sound of hit or restart the game immediately.</p>
+                <p class="someTextwidthClas">And Pause, where you can go to home game directory, on/off music and sound of hit or restart the game immediately.<a style=" padding:50px 0;" href="ProjData/Tap.apk" download>Download</a></p>
                 <img src="./assets/Games/tapPause.png" alt="">
+            </div>
+            
+            
+            <a href="projects.html" style ="padding:50px;" >Back</a>
             </div>
             `;
         }
